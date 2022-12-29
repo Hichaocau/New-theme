c0c3b7d43d6c9328836a47446e7bf8cdb2fd1016
DatNT-BraveBits <datnt@bravebits.vn>
2022-12-29 16:55:21 +0700

    update style header

---
+++ assets/header.js
@@ -0,0 +1,22 @@
+const header_btn = document.querySelector('.js-menu-drawer-trigger')
+const header_nav = document.querySelector('.header__nav');
+const icon_close = document.querySelector('.drawer__btn-close');
+const overlay = document.querySelector('.drawer__overlay');
+
+header_btn.addEventListener('click', () => {
+  header_nav.classList.remove('translate100');
+  header_nav.classList.add('translate0');
+  overlay.classList.toggle('d-none');
+})
+
+icon_close.addEventListener('click', () => {
+  header_nav.classList.add('translate100');
+  header_nav.classList.remove('translate0');
+  overlay.classList.toggle('d-none');
+})
+
+overlay.addEventListener('click', () => {
+  header_nav.classList.add('translate100')
+  header_nav.classList.remove('translate0')
+  overlay.classList.toggle('d-none')
+})
