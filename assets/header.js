5b1a092f361aafb9342ebd7b8ae7a007e49844d9
DatNT-BraveBits <datnt@bravebits.vn>
2023-01-03 22:05:44 +0700

    update section header

--- assets/header.js
+++ assets/header.js
@@ -1,23 +1,27 @@
 const header_menu = document.querySelector('.js-menu-drawer-trigger')
 const header_nav = document.querySelector('.header__nav');
+const drawer_body = document.querySelector('.drawer__body');
+const drawer_wapper = document.querySelector('.drawer__wapper');
 const icon_close = document.querySelector('.drawer__btn-close');
 const overlay = document.querySelector('.drawer__overlay');
 
 function MenuDrawer() {
   header_menu.addEventListener('click', () => {
-    header_nav.classList.toggle('active');
-    header_nav.style.transition = "cubic-bezier(0.5, 0, 0.1, 1) 0.2s 0.2s"
+    drawer_body.innerHTML = header_nav.innerHTML;
+    drawer_wapper.classList.toggle('active');
     overlay.classList.toggle('active');
+    drawer_wapper.style.transition = "cubic-bezier(0.5, 0, 0.1, 1) 0.2s 0.05s"
   })
   icon_close.addEventListener('click', () => {
-    header_nav.classList.toggle('active');
-    header_nav.style.transition = "cubic-bezier(0.9,0,0.5,1) 0.2s"
+    drawer_wapper.classList.toggle('active');
     overlay.classList.toggle('active');
+    drawer_wapper.style.transition = "cubic-bezier(0.9,0,0.5,1) 0.2s"
   })
   overlay.addEventListener('click', () => {
-    header_nav.classList.toggle('active');
-    header_nav.style.transition = "cubic-bezier(0.9,0,0.5,1) 0.2s"
+    drawer_wapper.classList.toggle('active');
     overlay.classList.toggle('active')
+    drawer_wapper.style.transition = "cubic-bezier(0.9,0,0.5,1) 0.2s"
   })
 }
 MenuDrawer()
+
