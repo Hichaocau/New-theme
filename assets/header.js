a4e57e09550e983b2afa37a10f24f9bd2585d646
DatNT-BraveBits <datnt@bravebits.vn>
2023-01-03 20:57:55 +0700

    update style

--- assets/header.js
+++ assets/header.js
@@ -5,20 +5,17 @@ const overlay = document.querySelector('.drawer__overlay');
 
 function MenuDrawer() {
   header_menu.addEventListener('click', () => {
-    header_nav.classList.remove('translate100');
-    header_nav.classList.add('translate0');
+    header_nav.classList.toggle('active');
     header_nav.style.transition = "cubic-bezier(0.5, 0, 0.1, 1) 0.2s 0.2s"
     overlay.classList.toggle('active');
   })
   icon_close.addEventListener('click', () => {
-    header_nav.classList.add('translate100');
-    header_nav.classList.remove('translate0');
+    header_nav.classList.toggle('active');
     header_nav.style.transition = "cubic-bezier(0.9,0,0.5,1) 0.2s"
     overlay.classList.toggle('active');
   })
   overlay.addEventListener('click', () => {
-    header_nav.classList.add('translate100')
-    header_nav.classList.remove('translate0')
+    header_nav.classList.toggle('active');
     header_nav.style.transition = "cubic-bezier(0.9,0,0.5,1) 0.2s"
     overlay.classList.toggle('active')
   })
