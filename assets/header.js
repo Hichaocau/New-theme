e3156fad9e54687578e3bf38536195748e42171e
DatNT-BraveBits <datnt@bravebits.vn>
2023-01-12 17:13:00 +0700

    fix bug QA

--- assets/header.js
+++ assets/header.js
@@ -29,3 +29,27 @@ function MenuDrawer() {
 }
 MenuDrawer()
 
+function headerScroll() {
+  let lastScroll = 0;
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      body.classList.remove('scroll-up');
+      return;
+    }
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+      // down
+      body.classList.remove('scroll-up');
+      body.classList.add("scroll-down");
+    } else if (
+      currentScroll < lastScroll &&
+      body.classList.contains("scroll-down")
+    ) {
+      // up
+      body.classList.remove("scroll-down");
+      body.classList.add('scroll-up');
+    }
+    lastScroll = currentScroll;
+  });
+}
+headerScroll()
